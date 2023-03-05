35c9a78dfcbd19c3cb49ff037bc2a25deda4acc7
jeanettesgithub <126692328+jeanettesgithub@users.noreply.github.com>
2023-03-05 22:46:11 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -67,7 +67,7 @@ function App() {
     <Screens />
   ) : (
     <>
-      <Title>Quick Notes</Title>
+      <Title>DynaStor</Title>
       <Authenticator
         onStateChange={authState => {
           if (authState === "signedIn") {
